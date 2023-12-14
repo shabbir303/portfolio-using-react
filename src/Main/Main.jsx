ea0c13a7d4cab0b3aee1d9af682751d0a24efa49
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2023-12-15 03:42:32 +0600

    video added successfully

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,5 +1,7 @@
 // import React from 'react';
-import video from "../assets/video (2160p).mp4"
+import video from "../assets/video (2160p).mp4";
+import video1 from "../assets/pexels-gamol-12973197 (1080p).mp4";
+import video2 from "../assets/video (2160p) (1).mp4"
 import bg from '../assets/blue-programming-code-background-abstract-footage-090894338_prevstill.webp'
 import { Link, Outlet } from "react-router-dom";
 import Navbar from "../Pages/Header/Navbar";
@@ -8,16 +10,16 @@ import img from '../assets/sabbir.png'
 import Animate from "../Pages/Animate";
 const Main = () => {
 
-  
+
     return (
-        <div className=" lg:flex font-lekton bg-[url()] " >
-            {/* <div  >
-            <video src={video} autoPlay muted loop className="video-bg"/>
-            </div> */}
-            <div className='fixed lg:static sm:top-0 z-10 right-0 left-0 '>
-            <Navbar></Navbar>
+        <div className=" lg:flex font-lekton relative " >
+            <div className="" >
+                <video src={video2} autoPlay muted loop className="absolute top-0 left-0 w-full h-screen object-cover" />
             </div>
-            <div className="relative w-full  lg:w-[500px] h-screen lg:h-[700px]  lg:my-[100px]  lg:ml-[20px] rounded-xl bg-stone-800  text-white shadow-xl shadow-slate-900">
+            <div className='fixed  sm:top-0 z-10 right-0 left-0 lg:relative'>
+                <Navbar></Navbar>
+            </div>
+            <div className="relative w-full  lg:w-[500px] h-screen lg:h-[700px]  lg:my-[100px]  lg:ml-[20px] rounded-xl bg-stone-800  text-white shadow-xl shadow-slate-900 z-10">
                 <div className='overflow-hidden rounded-lg has-shadow '>
                     <img src={bg}
                         className='aspect-video h-[400px] lg:h-[350px] rounded-b-[100px] z-10'
@@ -30,19 +32,19 @@ const Main = () => {
                         <h3 className='text-3xl font-bold'>
                             Shabbir Hossain
                         </h3>
-                        
-                            <Animate></Animate>
-                        
-                        
+
+                        <Animate></Animate>
+
+
                         <div className="mt-[60px] flex border-t-[1px] border-orange-300 justify-between items-center py-[20px]">
                             <a className="text-white text-center  border-r-[1px] border-orange-300 w-[50%]" href="https://drive.google.com/u/0/uc?id=1QN5H4MuQOnb4S6c_2aA_bOuCKO8HmFTJ&export=download&format=xlsx"><h1 >Download CV</h1></a>
-                            
+
                             <Link className="text-white text-center  w-[50%]" to='/contact'><h1 >Contact</h1></Link>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="lg:w-[800px] lg:h-[500px] bg-stone-800  my-auto rounded-xl overflow-scroll">
+            <div className="lg:w-[800px] lg:h-[500px]  bg-stone-800  my-auto rounded-xl overflow-scroll relative ">
                 <Outlet></Outlet>
             </div>
         </div>
